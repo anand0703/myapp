907da36ddaab64791af90ba27c6c274a0795b9d9
anand0703 <aniketrkf489@gmail.com>
2022-06-15 16:06:24 +0530

    15/6/22

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
 var studentRouter = require('./routes/student');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
 app.use('/student', studentRouter);
 app.use('/users', usersRouter);
 
--- routes/student.js
+++ routes/student.js
@@ -8,11 +8,30 @@ router.get('/', function (req, res, next) {
     res.json({ name: "aniket anand" })
 });
 
+router.get('/login', function (req, res, next) {
+    console.log(req.query)
+    // res.send({"name":"Aniket"});
+    res.render('student');
+    // res.json({ "Name": "What's up" });
+  });
+
 router.get('/:da-:ta', function (req, res, next) {
     console.log(req.params.da)
     // res.json({ name: "aniket anand" })
     res.render('student', { name: req.params.da, name1:req.params.ta});
 });
+router.get('/getLogin', function (req, res, next) {
+    console.log(req.query)
+    con.connect()
+    con.query("select * from form", function (err, result) {
+      if (err) throw err;
+      console.log(result);
+      res.render('login', { "data": result })
+    });
+    router.post('/updateLogin', function (req, res, next) {
+        console.log(req.body)
+        res.json({ "Name": "Hello" })
+      });
 
 
 router.get('/:da', function (req, res, next) {
